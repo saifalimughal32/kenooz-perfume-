b5ec2ea1d75d07b6bb8f9cccb3ff77e3e128dfb5
Lapteck <lapteck@macs-MacBook-Pro.local>
2026-05-21 23:19:45 +0500

    Remove intro splash screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
-import IntroSplash from "@/components/IntroSplash";
 import "./styles.css";
 
 import Index from "@/pages/Index";
@@ -20,28 +19,27 @@ import NotFound from "@/pages/NotFound";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        storageKey="kenooz-theme"
-        disableTransitionOnChange
-      >
-        <IntroSplash />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/brands" element={<Brands />} />
-            <Route path="/custom-fragrance" element={<CustomFragrance />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/manufacturing" element={<Manufacturing />} />
-            <Route path="/private-label" element={<PrivateLabel />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/scent-finder" element={<ScentFinder />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      storageKey="kenooz-theme"
+      disableTransitionOnChange
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/custom-fragrance" element={<CustomFragrance />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/manufacturing" element={<Manufacturing />} />
+          <Route path="/private-label" element={<PrivateLabel />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/scent-finder" element={<ScentFinder />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
